f09e9470dd19c0669a239f1563727e58d9ee3f2c
hongjinn <hongjinnpark@gmail.com>
2020-12-28 16:48:57 -0800

    round robin working!

--- myapp/entrypoint2.sh
+++ myapp/entrypoint2.sh
@@ -3,4 +3,4 @@
 python myapp/manage.py migrate --no-input
 python myapp/manage.py collectstatic --no-input
 
-gunicorn --bind 0.0.0.0:8000 --chdir /myproject/myapp myapp.wsgi:application
+gunicorn --bind 0.0.0.0:8002 --chdir /myproject/myapp myapp.wsgi:application
